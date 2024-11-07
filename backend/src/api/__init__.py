ffd4a6ca1b84c217db0fd7ff428a702c3a122da1
MrCode200 <navidyaghmaei@gmail.com>
2024-11-07 17:08:19 +0100

    removed __all__ from inits

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,3 +1,1 @@
 from .fetchData import fetch_historical_data
-
-__all__ = ["fetch_market_chart"]
--- backend/src/app.py
+++ backend/src/app.py
@@ -14,12 +14,10 @@ def init_app():
 logger = init_app()
 
 # Fetch market chart data
-data_frame = fetch_historical_data("XRP-USD", '1mo', "1h")
+data_frame = fetch_historical_data("BTC-USD", '1y', "1h")
 if data_frame is None:
     exit(1)
 
-signalSMA = SimpleMovingAverage.backtest(data_frame, short_period=5, long_period=20)
-signalRSI = SimpleMovingAverage.backtest(data_frame, short_period=14, long_period=50)
+signalSMA = SimpleMovingAverage.backtest(data_frame, short_period=3, long_period=10)
+signalRSI = RelativeStrengthIndex.backtest(data_frame, period=14, lower_band=15, upper_band=85)
 print(f"{signalRSI = }, {signalSMA = }")
-
-#signalRSI = np.float64(1.238658743286133), signalSMA = np.float64(1.015741122619629)
