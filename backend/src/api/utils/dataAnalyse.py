0c3ab2d1aaa768023fe7b46b38e10df9b9aadc71
kian19955 <kiansharifi84@gmail.com>
2024-11-09 18:01:44 +0330

    3H CONVERT COMPLETE

--- backend/src/api/utils/dataAnalyse.py
+++ backend/src/api/utils/dataAnalyse.py
@@ -1,5 +1,6 @@
 from backend.src.api import fetch_historical_data
 import pandas as pd
+
 def getData(ticker,period, interval):
     """
      The first three char of interval  are numbers => 00
@@ -8,19 +9,23 @@ def getData(ticker,period, interval):
     if(interval[3]=="m"):
         data_frame = fetch_historical_data(ticker, period, "1m")
         return data_frame
+
     elif(interval[3] == "h"):
         data_frame = fetch_historical_data(ticker, period, "1h")
-        #for n in range(1,len(data_frame)+1,3):
-        timestr =data_frame.index[0].strftime('%Y-%m-%d %X')
-        data = {
+        dt = pd.DataFrame()
+        for n in range(0,int((len(data_frame)+1)/int(interval[2])),3):
+            timestr =data_frame.index[n].strftime('%Y-%m-%d %X')
+            idx=[pd.Timestamp(year=int(timestr[0:4]), month=int(timestr[5:7]), day=int(timestr[8:10]), hour=int(timestr[11:13]),minute=int(timestr[14:16]),second=int(timestr[17:19]))]
 
-            "Datetime": [pd.Timestamp(year=int(timestr[0:4]), month=int(timestr[5:7]), day=int(timestr[8:10]), hour=int(timestr[11:13]),minute=int(timestr[14:16]),second=int(timestr[17:19]))],
-            "Open": [0.042],
-            "Close": [3.14],
-
-        }
-        dt=pd.DataFrame(data)
+            data = {
+                "Open": [data_frame.Open.iloc[n]],
+                "Close": [data_frame.Close.iloc[n+2]],
+                 "High" :[data_frame.High.iloc[n:n+3].max()],
+                "Low": [data_frame.Low.iloc[n:n+3].min()]
+            }
+            #print(pd.DataFrame(data,index=pd.Index(idx)))
+            dt=dt._append(pd.DataFrame(data,index=pd.Index(idx)))
         return dt
 
-d=getData("ADA-USD", '1d', "001h")
+d=getData("ADA-USD", '1mo', "003h")
 print(d)
