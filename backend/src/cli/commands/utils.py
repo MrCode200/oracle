bfa0c2b64da8c7acf5b8985f975d83cb8400f27a
MrCode200 <navidyaghmaei@gmail.com>
2024-12-28 19:47:17 +0100

    Improved and fixed problems with creating and editing object settings

--- backend/src/cli/commands/utils.py
+++ backend/src/cli/commands/utils.py
@@ -1,9 +1,12 @@
-from inspect import signature
+from inspect import signature, Signature
 from typing import Optional
+import ast
+from enum import Enum
 
 from rich.console import Console
 from rich.panel import Panel
-from rich.prompt import Prompt
+from rich.prompt import Prompt, Confirm
+from rich.text import Text
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from rich.table import Table
@@ -11,12 +14,15 @@ from rich.box import ROUNDED
 
 console = Console()
 
-def create_param_table(class_kwargs: dict[str, any]) -> Table:
+
+def create_param_table(params: dict[str, any], types: Optional[dict[str, any]] = None) -> Table:
     """
     Create a table with the parameters of the object
 
-    :param class_kwargs: The parameters of the object
+    :param params: The parameters of the object
+    :param types: The types of the parameters
     """
+    print(types)
     param_table = Table(header_style="bold cyan", box=ROUNDED, style="bold")
     param_table.add_column("", style="dim")
     param_table.add_column("Parameter", style="bold green")
@@ -24,11 +30,17 @@ def create_param_table(class_kwargs: dict[str, any]) -> Table:
     param_table.add_column("Type", style="bold magenta")
 
     i: int = 1
-    for param_name, param in class_kwargs.items():
-        param_table.add_row(str(i), param_name, str(param), str(param.__class__.__name__))
+    for param_name, param in params.items():
+        if types is not None:
+            param_type: str = str(types[param_name])
+        else:
+            param_type: str = str(param.__class__.__name__)
+        param_text: Text = Text(param_type)
+        param_table.add_row(str(i), param_name, str(param), param_text)
         i += 1
 
     return param_table
+
 
 def create_edit_object_settings(obj: object, settings: Optional[dict[str, any]] = None):
     """
@@ -36,63 +48,165 @@ def create_edit_object_settings(obj: object, settings: Optional[dict[str, any]] 
 
     :param obj: The object to be initialized.
     :param settings: A dictionary of keyword arguments to be passed to the object's constructor.
-                               If not provided, the function will create a dictionary with default values (excluding 'self').
+                    If not provided, the function will create a dictionary with default values (excluding 'self').
 
     :return: A dictionary of keyword arguments to be passed to the object's constructor.
     """
-    params = signature(obj.__init__).parameters
+    params = signature(obj).parameters
 
     if settings is None:
         settings: dict[str, any] = {}
 
         for param_name, param in params.items():
-            if param_name != "self":
-                settings[param_name] = param.default
+            settings[param_name] = param.default
 
-    console.print(create_param_table(settings))
+    types: dict[str, any] = {}
+    for param_name, param in params.items():
+        types[param_name] = param.annotation
+
+    if settings == {}:
+        return settings
+
+    console.print(create_param_table(settings, types=types))
 
     console.print(Panel(
         "[bold yellow]Type the name of the [underline white]`Parameter`[/underline white] you want to change:\n"
         "When you're done, press [underline white]`enter`[/underline white]\n"
         "To view all parameters, type [underline white]`VIEW`[/underline white]\n"
-        "For bools, [underline white]1 is True[/underline white] and [underline white]0 is False",
+        "Enums Options will be displayed.",
         title="INFO", box=ROUNDED, expand=False, border_style="bold bright_blue"
     ))
 
     while True:
-        param_name = prompt("Parameter: ",
-                            completer=WordCompleter(list(settings.keys()) + ["VIEW"], ignore_case=True))
-        if param_name == "VIEW":
-            console.print(create_param_table(settings))
-        elif param_name == "":
-            break
+        user_input = prompt("Parameter: ",
+                            completer=WordCompleter(list(settings.keys()) + ["VIEW"],
+                                                    ignore_case=True))
+        if user_input == "VIEW":
+            console.print(create_param_table(settings, types=types))
+
+        elif user_input == "":
+            empty_parameter_flag: bool = False
+
+            for name, value in settings.items():
+                if value == Signature.empty:
+                    console.print(
+                        f"[bold red]Error:[/bold red] Parameter '[underline grey]{name}[/underline grey]' cannot be '[underline grey]{Signature.empty}[/underline grey]' is not a valid parameter.")
+                    empty_parameter_flag = True
+                    break
+
+            if not empty_parameter_flag:
+                confirmation = Confirm.ask(
+                    "Are you sure you want to apply these changes?",
+                    choices=["y", "n"],
+                    default="y"
+                )
+                if confirmation == "y":
+                    return settings
 
         else:
-            if param_name in settings:
+            if user_input in settings:
+                param_name: str = user_input
+                param_value = settings[param_name]
+                param_annotation = params[param_name].annotation
+
+                # Check if the parameter is an Enum type
+                if isinstance(param_annotation, type) and issubclass(param_annotation, Enum):
+                    enum_values = [member.name for member in param_annotation]
+                    console.print(f"[bold green]Enum options for {param_name}[/bold green]: {', '.join(enum_values)}")
+
                 param_value = Prompt.ask(f"New value for [bold green]{param_name}[/bold green]:",
-                                         default=str(settings[param_name]))
+                                         default=str(param_value))
 
-                if params[param_name].annotation == bool:
-                    if param_value not in ["1", "0"]:
-                        console.print(
-                            f"[bold red]Error:[/bold red] Parameter '[underline grey]{param_name}[/underline grey]' must be 1 or 0.",
-                            style="red")
-                        continue
-                    param_value = True if param_value == "1" else False
-
-                elif params[param_name].annotation == int:
-                    try:
-                        param_value = int(param_value)
-                    except ValueError:
-                        console.print(
-                            f"[bold red]Error:[/bold red] Parameter '[underline grey]{param_name}[/underline grey]' must be an integer.",
-                            style="red")
-                        continue
-
-                settings[param_name] = param_value
+                try:
+                    print(types[param_name])
+                    settings[param_name] = convert_to_type(param_value, types[param_name], fallback=param_value)
+                except (ValueError, SyntaxError) as e:
+                    console.print(f"[bold red]Error:[/bold red] {e}")
             else:
                 console.print(
-                    f"[bold red] Parameter '[underline grey]{param_name}[/underline grey]' is not a valid parameter.[/bold red]"
+                    f"[bold red] Parameter '[underline grey]{user_input}[/underline grey]' is not a valid parameter.[/bold red]"
                 )
 
-    return settings
+def convert_to_type(
+        value: any,
+        target_type: type,
+        fallback: any = None,
+        raise_exception: bool = True
+) -> any:
+    """
+    Convert a value to a specific type.
+    Uses the `ast.literal_eval` function to evaluate the value of basic types (str not included).
+
+    Supports:
+    - Enum members by name or value
+    - Conversion to basic types like int, str, float, bool, etc.
+    - List, Dict, Tuple and Optional as parameterized generics
+    - Union of basic types
+
+    :param value: The value to convert.
+    :param target_type: The type to convert the value to.
+    :param fallback: The value to return if the conversion fails.
+    :param raise_exception: Whether to raise an exception if the value cannot be evaluated.
+    :return: The converted value or the fallback value.
+    """
+    # Check if Enum
+    if isinstance(target_type, type) and issubclass(target_type, Enum) and target_type is not Enum:
+        valid_names = [member.name for member in target_type]
+        valid_values = [member.value for member in target_type]
+
+        if value in valid_names:
+            return target_type[value]
+        elif value in valid_values:
+            return target_type(value)
+        else:
+            if raise_exception:
+                raise ValueError(f"Value '{value}' is not a valid member of enum '{target_type.__name__}'.")
+
+            return fallback
+
+    # Turn to basic type
+    if target_type == str:
+        return str(value)
+
+    try:
+        evaluated_value: any = ast.literal_eval(value)
+    except (ValueError, SyntaxError) as e:
+        if raise_exception:
+            raise e
+        else:
+            return fallback
+
+    # Check if basic type
+    try:
+        if isinstance(evaluated_value, target_type):
+            return evaluated_value
+        else:
+            if raise_exception:
+                raise ValueError(f"Value '{value}' cannot be converted to type '{target_type}'.")
+
+            return fallback
+    except TypeError as e:
+        pass
+
+    # Check if parameterized generic
+    try:
+        if hasattr(target_type, "__origin__"):
+            origin: type = target_type.__origin__
+            if origin == list:
+                item_type: type = target_type.__args__[0]
+                return [convert_to_type(item, item_type) for item in evaluated_value]
+
+            elif origin == dict:
+                key_type, item_type = target_type.__args__
+                return {convert_to_type(key, key_type): convert_to_type(item, item_type)
+                        for key, item in evaluated_value.items()}
+
+            elif origin == tuple:
+                item_type: type = target_type.__args__[0]
+                return tuple(convert_to_type(item, item_type) for item in evaluated_value)
+
+    except Exception as e:
+        if raise_exception:
+            raise e
+        else:
+            return fallback
