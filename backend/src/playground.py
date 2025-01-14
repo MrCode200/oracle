d5ffd5b65f4f1f61972fda49010ad9a51795d9d6
MrCode200 <navidyaghmaei@gmail.com>
2025-01-14 15:16:59 +0100

    fix(config): disable stream handler in logs
    • disable stream handler for cleaner logs
    • remove stream output to avoid clutter
    • update playground with evaluation tests

--- backend/src/playground.py
+++ backend/src/playground.py
@@ -1,5 +1,7 @@
+from src.api import fetch_klines
 from src.app import init_app
-from multiprocessing import Process
+from src.services.entities.tradingComponents import MovingAverageConvergenceDivergence, SimpleMovingAverage
+
 init_app()
 
 if __name__ == '__main__':
@@ -7,7 +9,12 @@ if __name__ == '__main__':
     from src.services.entities import Profile
 
     prof: Profile = profile_registry.get(1)
-    process = Process(target=prof.backtest, args=(1_000_000, 7, 1))
-    result = process.start()
-    process.join()
+    # result = prof.backtest(1_000_000, 7, 7)
+
+    df = fetch_klines(ticker="BTCUSDT", interval="1h", days=7)
+    result2 = prof.trading_components[0].instance.evaluate(df=df)
+    sma = SimpleMovingAverage(short_period=14, long_period=50, return_crossover_weight=True)
+    macd = MovingAverageConvergenceDivergence(slow_period=26, fast_period=12, signal_line_period=9, weight_impact=0)
+    result = macd.evaluate(df=df)
     print(result)
+    print(result2)
